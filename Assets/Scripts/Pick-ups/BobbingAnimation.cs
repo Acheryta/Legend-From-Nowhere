9401510ba51bd4aff532808b30ff126ceabc50a2
Acheryta <146699543+Acheryta@users.noreply.github.com>
2024-12-27 15:39:55 +0700

    Add Evolve Weapon System
    testing testing

--- Assets/Scripts/Pick-ups/BobbingAnimation.cs
+++ Assets/Scripts/Pick-ups/BobbingAnimation.cs
@@ -8,17 +8,22 @@ public class BobbingAnimation : MonoBehaviour
     public float magnitude; //Range of movement
     public Vector3 direction; //Direction of movement
     Vector3 initialPosition;
+    Pickup pickup;
 
     // Start is called before the first frame update
     void Start()
     {
+        pickup = GetComponent<Pickup>();
         initialPosition = transform.position;    
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Using Sin for smooth bobbing effect
-        transform.position = initialPosition + direction * Mathf.Sin(Time.time * frequency) * magnitude;
+        if(pickup && !pickup.hasBeenCollected)
+        {
+            //Using Sin for smooth bobbing effect
+            transform.position = initialPosition + direction * Mathf.Sin(Time.time * frequency) * magnitude;
+        }
     }
 }
